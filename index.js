cac1489a3fa750ef3c76e43884f3331bb51f60b5
Yousan_O <yousan@l2tp.org>
2019-12-08 20:24:23 +0900

    Add basic settings

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import GSSCSVUrl from 'gss-csv-url';
+
+const gss_csv_url = new GSSCSVUrl();
+
+(() => {
+  // const gss_csv_url = new GSSCSVUrl();
+  const url2 = 'https://docs.google.com/spreadsheets/d/1yfMIdt8wgBPrMY3UwiCTsX3EN_2gcLCmPAEy8dfYeLY/edit#gid=374636390';
+  console.log(gss_csv_url)
+  console.log(gss_csv_url.url(url2))
+
+  // const obj = new GSSCSVUrl();
+// console.log(gss_csv_url.gss_url(url));
+  //const obj = new gss_csv_url()
+  // console.log(obj.url('abc'))
+
+  const input_url = 'https://docs.google.com/spreadsheets/d/1yfMIdt8wgBPrMY3UwiCTsX3EN_2gcLCmPAEy8dfYeLY/edit#gid=374636390';
+  // const csv_url = chalk.url(input_url);
+
+// would be 'https://docs.google.com/spreadsheets/d/1yfMIdt8wgBPrMY3UwiCTsX3EN_2gcLCmPAEy8dfYeLY/export?format=csv&gid=374636390'
+  console.log(csv_url);
+  const url = process.argv[2]
+  console.log(process.argv[2])
+  const dlable = gss_csv_url.url
+  console.log(dlable)
+})()
